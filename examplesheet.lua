1bbaca7ec32cc7921714b8920b6c588bf1b968a6
GitHubTestei <109692029+GitHubTestei@users.noreply.github.com>
2022-07-20 11:58:39 -0700

    Create examplesheet.lua

---
+++ examplesheet.lua
@@ -0,0 +1,10 @@
+-- Sheet for Ocean Eyes by Billie Eilish
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 9*40 + 25 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[
+[8f] [9d] [0a] [8f] [9d] [0a] [8f] [9d] [0a] [wa] [9d] [0u] - [8f] [9d] [0a] p o [8f] [9d] [0a] [8f] [9d] [0a] p o p [wa] [0d] [0u] - [8f] [9d] [0a] p o [8f] [9d] [0s] a [8f] [9d] [0a] p o p [wa] [9d] [0u] - a o 8 9 0 p o [8p] o 9 0 u u [8a] a a [9p] p p o 0 u u o o p [wa] [9d] [0a] - a o 8 9 0 p o [8p] o 9 0 u u [8a] a a [9p] p p o 0 u u o o p [wa] [9d] [0a] - p [wa] [9d] [0f]
